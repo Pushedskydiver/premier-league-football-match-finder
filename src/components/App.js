4acd6caa49ef6ad3bf7565416a00d96c574c3627
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2019-08-18 22:08:49 +0100

    :sparkles: Setup api to get team standings

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ const apiValue = '64ec5fbc22msh6cdc0762a2c3439p103074jsn89f2b5f63a62';
 const headers = { headers: { 'x-rapidapi-key': apiValue } };
 
 class App extends Component {
+  state = {
+    standings: []
+  }
+
   fetchData = async api => {
     const response = await fetch(api, headers);
     const body = await response.json();
@@ -13,34 +17,40 @@ class App extends Component {
       throw Error(body.message);
     }
 
-    return body.api.teams;
+    return body.api;
   };
 
   storeTeamData(data) {
-    console.log({ data });
+    const standings = data.standings[0];
+
+    this.setState({ standings });
+    localStorage.setItem('standings', JSON.stringify(standings));
   }
 
-  componentDidMount() {
-    this.fetchData(`${api}teams/league/524`)
+  async componentDidMount() {
+    await this.fetchData(`${api}leagueTable/524`)
       .then(data => this.storeTeamData(data))
       .catch(error => console.error(error));
   }
 
-  render() {
+  renderTeam({ points, rank, teamName, logo }, key) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div key={key}>
+        <p>Rank: {rank}</p>
+        <p>Team: {teamName}</p>
+        <p>Points: {points}</p>
+        <img src={logo} alt={`${teamName} logo`} />
       </div>
+    );
+  }
+
+  render() {
+    const standings = JSON.parse(localStorage.getItem('standings')) || this.state.standings;
+
+    return (
+      <header className="App-header">
+        {standings.map((team, key) => this.renderTeam(team, key))}
+      </header>
     );
   }
 }
