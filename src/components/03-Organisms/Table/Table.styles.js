85d6094bfd2ff281fa609ae1d6fe19cc00f699e2
Alex Clapperton <Pushedskydiver@users.noreply.github.com>
2019-08-24 21:04:37 +0100

    :lipstick: Update table

--- src/components/03-Organisms/Table/Table.styles.js
+++ src/components/03-Organisms/Table/Table.styles.js
@@ -1,6 +1,7 @@
 import styled, { css } from 'styled-components';
 import typography from '../../../styles/settings.typography.styles';
 import { maxWidth, minWidth } from '../../../styles/settings.breakpoints.styles';
+import spacing from '../../../styles/settings.spacing.styles';
 
 export const TableStyles = styled.table`
   width: 100%;
@@ -27,14 +28,16 @@ export const THeader = styled.th`
 
 export const TRow = styled.tr`
   display: grid;
-  grid-template-columns: minmax(20px, 0.5fr) 4fr repeat(5,1fr);
+  grid-template-columns: minmax(20px, 0.5fr) 4fr repeat(5, 1fr);
 
   ${maxWidth('768', () => css`
     grid-column-gap: 10px;
   `)}
 
   ${minWidth('768', () => css`
-    grid-template-columns: minmax(20px, 0.5fr) 4fr repeat(6,1fr);
+    grid-template-columns: minmax(20px, 0.5fr) 4fr repeat(6, 1fr);
+    padding-top: ${spacing.padding / 4}px;
+    padding-bottom: ${spacing.padding / 4}px;
   `)}
 `;
 
@@ -47,7 +50,7 @@ export const TData = styled.td`
 
   ${props => props.last3 && css`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: repeat(3, minmax(30px, 1fr));
     grid-column-gap: 5px;
   `}
 
